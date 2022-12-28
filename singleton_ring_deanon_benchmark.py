c9bd0c013854bd0786e0dffb1001f139f1760c96
Mitchell Krawiec-Thayer <mitchellpkt+github@protonmail.com>
2022-12-28 18:30:34 -0500

    feat: double speed with set native approach. Processed 4525604048 ring pairs in 57.32 seconds

--- singleton_ring_deanon_benchmark.py
+++ singleton_ring_deanon_benchmark.py
@@ -3,7 +3,7 @@
 # Expect extra overhead for a full implementation and intra-transaction rings
 
 from load_transactions import get_processed_transaction
-from typing import Dict, List, Tuple
+from typing import Dict, List, Tuple, Set
 from multiprocessing import Pool
 from time import perf_counter
 
@@ -13,8 +13,8 @@ n_workers: int = 64
 # Pick two transactions
 tx_hash_left: str = "48ab24a942778d0c7d79d8bbc7076329ae45b9b7c8cc7c15d105e135b4746587"
 tx_hash_right: str = "71879ba6099ea18d456cd31694b0860f3649ebeb28ce5630ccb1be312c0cc8cb"
-tx_rings_left: Dict[str, List[str]] = get_processed_transaction(tx_hash_left)
-tx_rings_right: Dict[str, List[str]] = get_processed_transaction(tx_hash_right)
+tx_rings_left: Dict[str, Set[str]] = {k: set(v) for k, v in get_processed_transaction(tx_hash_left).items()}
+tx_rings_right: Dict[str, Set[str]] = {k: set(v) for k, v in get_processed_transaction(tx_hash_right).items()}
 
 
 def wrapper(rings_left, rings_right):
@@ -22,17 +22,15 @@ def wrapper(rings_left, rings_right):
     for key_image_left, ring_left in rings_left.items():
         for key_image_right, ring_right in rings_right.items():
             # Do we have a singleton?
-            if len(set(ring_left) & set(ring_right)) == len(ring_left) - 1:
-                left_singleton: str = (set(ring_left) - set(ring_right)).pop()
-                right_singleton: str = (set(ring_right) - set(ring_left)).pop()
-                results.append((key_image_left, left_singleton))
-                results.append((key_image_right, right_singleton))
+            if len(ring_left & ring_right) == len(ring_left) - 1:
+                results.append((key_image_left, (ring_left - ring_right).pop()))
+                results.append((key_image_right, (ring_right - ring_left).pop()))
     return results
 
 
-all_rings_left: List[Dict[str, List[str]]] = [tx_rings_left] * n_samples
-all_rings_right: List[Dict[str, List[str]]] = [tx_rings_right] * n_samples
-iterator: List[Tuple[Dict[str, List[str]], Dict[str, List[str]]]] = list(zip(all_rings_left, all_rings_right))
+all_rings_left: List[Dict[str, Set[str]]] = [tx_rings_left] * n_samples
+all_rings_right: List[Dict[str, Set[str]]] = [tx_rings_right] * n_samples
+iterator: List[Tuple[Dict[str, Set[str]], Dict[str, Set[str]]]] = list(zip(all_rings_left, all_rings_right))
 
 tic: float = perf_counter()
 with Pool(n_workers) as p:
