bba066b665cd35cd188f88e1b727aa44616d1fe7
Mitchell Krawiec-Thayer <mitchellpkt+github@protonmail.com>
2022-12-28 18:40:08 -0500

    tweak: update back of envelope

--- back_of_envelope.py
+++ back_of_envelope.py
@@ -1,7 +1,8 @@
 num_rings: int = 5_573_404
-pairwise_rate_Hz: float = 4_500_000_000 / 102
+pairwise_rate_Hz: float = 4_525_604_048 / 53
 num_checks: int = int((num_rings**2) / 2)
 estimated_time_sec: float = num_checks / pairwise_rate_Hz
+print(f"Check rate (Hz): {pairwise_rate_Hz:.2f}")
 print(f"Number of ring pair checks: {num_checks}")
 print(f"Estimated time: {estimated_time_sec:.2f} seconds")
 print(f"Estimated time: {estimated_time_sec / (24 * 60 * 60):.2f} days")
