2304aab13e94bce0a684f6f90ebe1530003eb758
Mitchell Krawiec-Thayer <mitchellpkt+github@protonmail.com>
2022-12-30 15:26:34 -0500

    feat: parallelized API

--- ringxor/ringxor.py
+++ ringxor/ringxor.py
@@ -1,6 +1,9 @@
 from typing import Type, TypeVar, Dict, Set, List, Tuple, Union, Any, Collection
 import itertools
+from multiprocessing import Pool, cpu_count
+import numpy as np
 
+# This aliasing is temporary, until the schema is finalized
 key_image_pointer: Type = Union[int, str, Any]
 output_pointer: Type = Union[int, str, Any]
 ring_vector: Type = Set[output_pointer]
@@ -8,12 +11,20 @@ ring_bucket: Type = Dict[key_image_pointer, ring_vector]
 edge: Type = Tuple[key_image_pointer, output_pointer]
 
 
-def process_bucket_single_thread(
-    rings: ring_bucket, index_pairs: Collection[Tuple[int, int]] = None,
+def process_bucket_single_thread_core(
+    rings: ring_bucket,
+    index_pairs: Union[List[Tuple[int, int]], None],
 ) -> List[Tuple[key_image_pointer, output_pointer]]:
+    """
+    Core function - you do not need to interact with this directly, use `process_bucket()` below
+    This takes a batch of index pairs and processes the rings in a single thread.
 
-    # If no index pairs are provided, use all possible combinations
-    if index_pairs is None:
+    :param rings: bucket of rings to analyze
+    :param index_pairs: list of index pairs to process. If None provided, checks all possible combinations
+    :return: identified transaction tree edges in the form of (key_image, output)
+    """
+    # If no index pairs are provided (could be None, (), {}, [], etc...), use all possible combinations
+    if not index_pairs:
         index_pairs = itertools.product(list(rings.keys()), list(rings.keys()))
 
     # Avoid redundant checks by only crunching the upper triangle of the index pair matrix
@@ -32,3 +43,51 @@ def process_bucket_single_thread(
             edges.append((key_image_pointer_right, (ring_right - ring_left).pop()))
 
     return edges
+
+
+def process_bucket(
+    rings: ring_bucket,
+    index_pairs: Union[None, Collection[Tuple[int, int]]] = None,
+    num_workers: Union[int, None] = None,
+) -> List[Tuple[key_image_pointer, output_pointer]]:
+    """
+    This function takes a bucket of rings and processes them to identify transaction tree edges from singletons
+    that arise from XORing ring pairs (i.e. symmetric set difference)
+
+    :param rings: bucket of rings to analyze
+    :param index_pairs: list of index pairs to process. If None provided, checks all possible combinations
+    :param num_workers: number of workers to use. If None provided, uses all available cores
+    :return: identified transaction tree edges in the form of (key_image, output)
+    """
+    # If no index pairs are provided, use all possible combinations
+    if index_pairs is None:
+        index_pairs = itertools.product(list(rings.keys()), list(rings.keys()))
+
+    # Avoid redundant checks by only crunching the upper triangle of the index pair matrix
+    key_image_pointer_pairs: List[Tuple[key_image_pointer, key_image_pointer]] = list(
+        {tuple(sorted(ip)) for ip in index_pairs}
+    )
+
+    # Use all available cores unless specified otherwise
+    if num_workers is None:
+        num_workers: int = cpu_count()
+
+    if num_workers <= 1:
+        # If we only have one worker, just run the single-threaded version
+        return process_bucket_single_thread_core(rings, index_pairs=key_image_pointer_pairs)
+    else:
+        # Split the work into chunks
+        batches = [list(key_image_pointer_pairs)[i::num_workers] for i in range(num_workers)]
+        iterable = [(rings, batch) for batch in batches]
+
+        # Process the chunks in parallel
+        with Pool(processes=num_workers) as pool:
+            results: List[List[Tuple[key_image_pointer, output_pointer]]] = pool.starmap(
+                process_bucket_single_thread_core,
+                iterable,
+            )
+        # Combine the results
+        edges: List[Tuple[key_image_pointer, output_pointer]] = []
+        for result in results:
+            edges.extend(result)
+        return edges
--- tests/test_ringxor.py
+++ tests/test_ringxor.py
@@ -1,10 +1,8 @@
-import pytest
 import json
-from ringxor import ringxor
-from itertools import product
 import os
+from multiprocessing import cpu_count
+from ringxor import ringxor
 
-print(f"{os.getcwd()=}")
 # Load the test data
 with open("demo_rings.json", "r") as f:
     all_rings_raw = json.load(f)
@@ -13,9 +11,8 @@ with open("demo_rings.json", "r") as f:
 all_rings: ringxor.ring_bucket = {key_image: set(ring) for key_image, ring in all_rings_raw.items()}
 
 
-def test_ringxor_process_bucket_single_thread():
-    # Run the ringxor algorithm
-    results = sorted(ringxor.process_bucket_single_thread(all_rings, index_pairs=None))
+def test_ringxor_process_bucket_single_thread_core():
+    results = sorted(ringxor.process_bucket_single_thread_core(all_rings, index_pairs=None))
 
     assert len(results) == 54
     assert results[0] == (
@@ -26,3 +23,34 @@ def test_ringxor_process_bucket_single_thread():
         "fdfcbe3f85e480905d5db681bf89e5b81dc514871f0735c8a8cc7dffb6d7cde8",
         "6d1fe6224bfccbb5f660d08bdb23fba89e75cf7d367b16bc40bcb3d337e12b0e",
     )
+
+
+def test_ringxor_process_bucket_1_worker():
+    results = sorted(ringxor.process_bucket(all_rings, index_pairs=None, num_workers=1))
+
+    assert len(results) == 54
+    assert results[0] == (
+        "056189f0c3ed7806bc17655152613b3288dfa496b6566b9bbef930efaa87975f",
+        "416e9438f9f69410cf42959dc8ee515318ea6710e5bf8eb76f69c6183a09bded",
+    )
+    assert results[53] == (
+        "fdfcbe3f85e480905d5db681bf89e5b81dc514871f0735c8a8cc7dffb6d7cde8",
+        "6d1fe6224bfccbb5f660d08bdb23fba89e75cf7d367b16bc40bcb3d337e12b0e",
+    )
+
+
+def test_ringxor_process_bucket_N_worker():
+    if cpu_count() == 1:
+        print("Skipping test_ringxor_process_bucket_N_worker because only 1 CPU is available")
+    else:
+        results = sorted(ringxor.process_bucket(all_rings, index_pairs=None, num_workers=2))
+
+        assert len(results) == 54
+        assert results[0] == (
+            "056189f0c3ed7806bc17655152613b3288dfa496b6566b9bbef930efaa87975f",
+            "416e9438f9f69410cf42959dc8ee515318ea6710e5bf8eb76f69c6183a09bded",
+        )
+        assert results[53] == (
+            "fdfcbe3f85e480905d5db681bf89e5b81dc514871f0735c8a8cc7dffb6d7cde8",
+            "6d1fe6224bfccbb5f660d08bdb23fba89e75cf7d367b16bc40bcb3d337e12b0e",
+        )
